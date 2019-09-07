0209fab7a396832d518638110057a58036129e22
hazelnutx <raulrusu46@gmail.com>
2019-09-07 14:38:08 +0300

    changed from IntelliJ to VS Code

--- src/GUI_Chapter_12/Animate.java
+++ src/GUI_Chapter_12/Animate.java
@@ -1,4 +1,5 @@
 package GUI_Chapter_12;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -6,13 +7,13 @@ public class Animate {
     int x = 1;
     int y = 1;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Animate gui = new Animate();
         gui.go();
     }
 
-    public void go(){
-        JFrame frame = new JFrame();
+    public void go() {
+        JFrame frame = new JFrame("Oh sheet!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         MyDrawP drawP = new MyDrawP();
@@ -20,23 +21,24 @@ public class Animate {
         frame.setSize(500, 270);
         frame.setVisible(true);
 
-        for(int i = 0; i< 124; i++, x++, y++){
+        for (int i = 0; i < 124; i++, x++, y++) {
             x++;
             drawP.repaint();
 
-            try{
+            try {
                 Thread.sleep(50);
-            } catch (Exception e){}
+            } catch (Exception e) {
+            }
         }
     }
 
-    class MyDrawP extends JPanel{
-        public void paintComponent(Graphics g){
+    class MyDrawP extends JPanel {
+        public void paintComponent(Graphics g) {
             g.setColor(Color.white);
-            g.fillRect(0,0, 500, 250);
+            g.fillRect(0, 0, 500, 250);
 
             g.setColor(Color.blue);
-            g.fillRect(x, y, 500-x*2, 250-y*2);
+            g.fillRect(x, y, 500 - x * 2, 250 - y * 2);
 
         }
     }
