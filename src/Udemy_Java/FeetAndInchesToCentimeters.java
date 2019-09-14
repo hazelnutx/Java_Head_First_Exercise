e5ccdd24e18e71cfd4679204a5cb82482481dd01
hazelnutx <raulrusu46@gmail.com>
2019-09-14 09:47:16 +0300

    finished the feet, inches to cm problem

--- src/Udemy_Java/FeetAndInchesToCentimeters.java
+++ src/Udemy_Java/FeetAndInchesToCentimeters.java
@@ -1,12 +1,14 @@
 package Udemy_Java;
 
 public class FeetAndInchesToCentimeters {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         calFeetAndInchesToCentimeters(5.7, 0);
+
+        calFeetAndInchesToCentimeters(100);
     }
 
-    public static double calFeetAndInchesToCentimeters(double feet, double inches){
-        if(feet < 0 || inches < 0 && inches > 12){
+    public static double calFeetAndInchesToCentimeters(double feet, double inches) {
+        if (feet < 0 || inches < 0 && inches > 12) {
             return -1;
         }
         double centimeters = (feet * 12) * 2.54;
@@ -15,10 +17,13 @@ public class FeetAndInchesToCentimeters {
         return centimeters;
     }
 
-    public static double calFeetAndInchesToCentimeters(double inches){
-        if(inches < 0){
+    public static double calFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
             return -1;
         }
-        return inches;
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is euqal to " + feet + " feet and " + remainingInches + " inches.");
+        return calFeetAndInchesToCentimeters(feet, remainingInches);
     }
 }
